5f29a360f1dac8945fa5414e318a6fac1438d8ff
sanjay kumar <sanjayjnu22@gmail.com>
2024-01-09 19:02:12 +0530

    Episode-2 Igniting our app with ParcelJs

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-/**
- * <div id='parent'>
- *  <div id = 'child1'>
- *      <h1>I am h1 tag </h1>
- *      <h1>I am h2 tag </h1>
- *  </div>
- * <div id = 'child2'>
- *      <h1>I am h1 tag </h1>
- *      <h1>I am h2 tag </h1>
- *  </div>
- * </div>
- */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 
 const parent = React.createElement(
     'div', 
     {id: 'parent'}, [
     React.createElement('div', {id: 'child1'}, [ 
-        React.createElement('h1', {}, 'I am h1 tag'),
+        React.createElement('h1', {}, 'I am test tag'),
         React.createElement('h2', {}, 'I am h2 tag')
     ]),
     React.createElement('div', {id: 'child2'}, [ 
